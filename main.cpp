63466f875d68d905a7d40dc525cfd604f5d57909
Alexis ANDREANI <alexis.andreani@alten.com>
2022-02-20 11:24:20 +0100

    user input options

--- main.cpp
+++ main.cpp
@@ -4,15 +4,37 @@
 #include "black_scholes.h"
 #include "newton_raphson.h"
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
-int main(int argc, char **argv) {
+int main() {
     // First we create the parameter list
-    double S = 100.0;  // Underlying spot price
-    double K = 100.0;  // Strike price
-    double r = 0.05;   // Risk-free rate (5%)
-    double T = 1.0;    // One year until expiry
-    double C_M = 10.5; // Option market price
 
+    // Input Values
+    cout << "Compute the implied vlatility of an option/n";
+    cout << "Please provide the value for the different parameters of your option/n";
+
+    double S = 100.0;  // Underlying spot price value by default
+    /*input ask the user some inputs*/
+    cout << "Enter the principal value: ";
+    cin >> S;
+
+    double K = 100.0;  // Strike price
+    cout << "Enter the strike price: ";
+    cin >> K;
+
+
+    double r = 0.05;   // Risk-free rate (5%)
+    cout << "Enter the risk-free rate: ";
+    cin >> r;
+
+    double T = 1.0;    // One year until expiry
+    cout << "Enter the maturity: ";
+    cin >> T;
+
+    double C_M = 10.5; // Option market price
+    cout << "Enter option market price: ";
+    cin >> C_M;
     // Create the Black-Scholes Call functor
     BlackScholesCall bsc(S, K, r, T);
 
@@ -25,6 +47,7 @@ int main(int argc, char **argv) {
             &BlackScholesCall::option_price,
             &BlackScholesCall::option_vega>
             (C_M, init, epsilon, bsc);
+
 
     // Output the values
     std::cout << "Implied Vol: " << sigma << std::endl;
